4f86470ab7970f602b6d925b04dc715a4a8b904c
ekoydakoykoy <ragnaniekoy@yahoo.com>
2020-12-02 18:48:28 +0800

    initial

---
+++ src/components/Todos.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div  v-for="todo in todos" :key="todo.id"> 
+            <TodoItem :todo="todo" @del-todo="$emit('del-todo', todo.id)" />
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: "Todos",
+    components: {
+        TodoItem
+    },
+    props: ["todos"]
+    // props: {
+    //     todos: {
+    //         type: Array,
+    //         required: true
+    //     }
+    // }
+        
+}
+</script>
+
+<style scoped>
+
+</style>
